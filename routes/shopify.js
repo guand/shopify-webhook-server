68dbee71e92ba21f03efa224851b6e5ae459f272
guand <jguan@terapeak.com>
2014-09-29 15:12:55 -0700

    added transactions

--- routes/shopify.js
+++ routes/shopify.js
@@ -5,7 +5,10 @@ exports.retrieveRequests = function(req, res){
 	// console.log(req);
 	var order = req.body,
 	items = [],
-	shopify_items = order.line_items;
+	shopify_items = order.line_items,
+	shipping = order.shipping_lines,
+	shipping_cost = 0,
+	date = new Date(order.created_at);
 
 	for(var i = 0, len = shopify_items.length; i < len; ++i){
 		var url_title = ((shopify_items[i].title).toLowerCase()).split(' ').join('-');
@@ -25,11 +28,47 @@ exports.retrieveRequests = function(req, res){
 		}
 		items.push(item_object);
 	}
+	for(var j = 0, len = shipping.length; j < len; ++j){
+		shipping_cost += shipping[j].price;
+	}
 
 	var shopify_transaction = {
-		
+		"id": order.id,
+		"time": date.getTime(),
+		"shipCost": shipping_cost,
+		"currencyISOCode": order.currency,
+		"type": "Fixed Price"
 	};
-	console.log(order);
+
+	var shopify_seller = {
+		"id": req.headers['x-shopify-shop-domain'];
+		"email": "",
+		"name": "",
+		"countryISOCode": ""
+	};
+
+	var shopify_buyer = {
+		"id": order.customer.id,
+		"email": order.customer.email,
+		"name": order.customer.name,
+		"countryISOCode": order.shipping_address.country_code,
+		"state": order.shipping_address.provice_code,
+		"city": order.shipping_address.city,
+		"age": "",
+		"gender": ""
+	}
+
+	var transactions = {
+		"transactions": [
+			"channelId": "shopify",
+			"items": items,
+			"transaction": shopify_transaction,
+			"seller": shopify_seller,
+			"buyer": shopify_buyer,
+			"source": "shopify"
+		]
+	}
+	console.log(transaction);
 	res.end("OK");
 }
 
