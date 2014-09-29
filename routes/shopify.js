4bf39abf63b4951752e8e52723d85603699ead3c
guand <jguan@terapeak.com>
2014-09-29 14:03:50 -0700

    added item object mapping

--- routes/shopify.js
+++ routes/shopify.js
@@ -3,10 +3,27 @@ var request = require('request');
 
 exports.retrieveRequests = function(req, res){
 	// console.log(req);
-	var order = req.body;
-	request('https://pie-eats.myshopify.com/admin/products/370123851.json', function(error, response, body){
-		console.log(body);
-	});
+	var order = req.body,
+	items = [],
+	shopify_items = order.line_items;
+
+	for(var i = 0, len = shopify_items.length; i < len; ++i){
+		var url_title = (shopify_items[i].title).split(' ').join('-');
+		var item_object = {
+			"itemId": shopify_items[i].id,
+			"title": shopify_items[i].title,
+			"quantityAvailable": "",
+			"quantitySold": shopify_items[i].quantity,
+			"itemPrice": shopify_items[i].price,
+			"productId": {
+				"value": shopify_items[i].sku,
+				"type": "sku"
+			}
+			"viewURL": "https://" + res.headers['x-shopify-shop-domain'] + "/products/" + url_title,
+			"categories": [];
+		}
+		items.push(item_object);
+	}
 	res.end("OK");
 }
 
