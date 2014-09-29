82435c4dce42def1bce0dfbdd7cd87dcb029cc9c
guand <jguan@terapeak.com>
2014-09-29 14:10:24 -0700

    added item object mapping

--- routes/shopify.js
+++ routes/shopify.js
@@ -7,24 +7,24 @@ exports.retrieveRequests = function(req, res){
 	items = [],
 	shopify_items = order.line_items;
 
-	for(var i = 0, len = shopify_items.length; i < len; ++i){
-		var url_title = (shopify_items[i].title).split(' ').join('-');
-		var item_object = {
-			"itemId": shopify_items[i].id,
-			"title": shopify_items[i].title,
-			"quantityAvailable": "",
-			"quantitySold": shopify_items[i].quantity,
-			"itemPrice": shopify_items[i].price,
-			"productId": {
-				"value": shopify_items[i].sku,
-				"type": "sku"
-			},
-			"viewURL": "https://" + res.headers['x-shopify-shop-domain'] + "/products/" + url_title,
-			"categories": []
-		}
-		items.push(item_object);
-	}
-	console.log(items);
+	// for(var i = 0, len = shopify_items.length; i < len; ++i){
+	// 	var url_title = (shopify_items[i].title).split(' ').join('-');
+	// 	var item_object = {
+	// 		"itemId": shopify_items[i].id,
+	// 		"title": shopify_items[i].title,
+	// 		"quantityAvailable": "",
+	// 		"quantitySold": shopify_items[i].quantity,
+	// 		"itemPrice": shopify_items[i].price,
+	// 		"productId": {
+	// 			"value": shopify_items[i].sku,
+	// 			"type": "sku"
+	// 		},
+	// 		"viewURL": "https://" + res.headers['x-shopify-shop-domain'] + "/products/" + url_title,
+	// 		"categories": []
+	// 	}
+	// 	items.push(item_object);
+	// }
+	console.log(res.headers["x-shopify-shop-domain"]);
 	res.end("OK");
 }
 
